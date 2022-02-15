f684df7e3c217ad70bead4ed7619f94bce5584e3
anygor <targemrazor@gmail.com>
2022-02-15 20:22:58 +0300

    Initital D

---
+++ src/test/java/ru/math/tversu/studentapp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.math.tversu.studentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
