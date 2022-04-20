051970c1cddf0fc9d22664f540bc4d0bea3019a8
Andrey Gordeev <andrey.gordeev@harley-davidson.com>
2022-04-20 13:36:52 +0300

    Add MySQL properties, integrate authorization with db

---
+++ src/main/java/ru/math/tversu/studentapp/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package ru.math.tversu.studentapp.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM user WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM user WHERE username=?")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+    }
+}
